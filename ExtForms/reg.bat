ed2905a5598418fc1b306a6f97a3503c207f68ec
phsin <phsin@kb99.pro>
2018-08-02 16:46:59 +0400

    Отправка ошибок в bugtracker sentry +dll

--- ExtForms/reg.bat
+++ ExtForms/reg.bat
@@ -1,2 +1,2 @@
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe soapdll_test.dll /codebase /tlb:soap_test.tlb /verbose
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe soapdll_test.dll /codebase /tlb:soap_test.tlb
 pause
