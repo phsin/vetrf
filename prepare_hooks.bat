3637f23e3eb459bff9a52ba8e000fac9c937704d
phsin <phsin@kb99.pro>
2018-11-08 12:27:32 +0400

    pre-commit в hooks для разбора ert

--- prepare_hooks.bat
+++ prepare_hooks.bat
@@ -2,10 +2,12 @@ cd .git\hooks
 del convert_utf8.sh
 del pyv8unpack.py
 del clean_dir2.bat
+del pre-commit
 
 rem %cd% - путь к текущей папке, без хвостового бэкслэша (\)
 rem %~dp0 - путь к папке из которой был запущен батник, заканчивается бэкслешем (\)
 mklink %~dp0.git\hooks\pyv8unpack.py %~dp0hooks\pyv8unpack.py 
 mklink %~dp0.git\hooks\convert_utf8.sh %~dp0hooks\convert_utf8.sh
 mklink %~dp0.git\hooks\clean_dir2.bat %~dp0hooks\clean_dir2.bat
+mklink %~dp0.git\hooks\pre-commit %~dp0hooks\pre-commit
 
