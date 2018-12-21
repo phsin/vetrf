00264aa0b813eea55bc35cf3023a5be10dc68bff
phsin <phsin@kb99.pro>
2018-12-21 18:04:05 +0400

    испр ТермическиеУсловия + адрес прокси

--- compile.bat
+++ compile.bat
@@ -2,4 +2,4 @@ rem Пересоберем ert в vetrf-repo\OUT
 cscript decompile_ert.js --ini-file compile-ert.ini
 rem compile MD
 gcomp -c -DD src\MD 
-copy 1cv7.new.md Vetis\1cv7_Vetis.md
+copy 1cv7.new.md 1cv7_Vetis.md
