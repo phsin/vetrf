0c0845ca054ea3d2d5c21ef2c481110e80e451ee
phsin <phsin@kb99.pro>
2019-01-21 14:06:36 +0400

    Удаление логов старше 30 дней refs #2395

--- del_logs.bat
+++ del_logs.bat
@@ -2,7 +2,5 @@ rem Удаление xml файлов старше 30 дней в каталоге
 
 set offset=30
 set logsDir="C:\1C\Complex\Vetis\logs\"
-rem pushd %logsDir% && (forfiles /d -%offset% /m *.xml /c "cmd /c del /f /q @file")
-rem popd
 cd  %logsDir%
 for /f "skip=%offset%" %%i in ('dir /o:-d /a:-d /b *.xml') do del %%i
