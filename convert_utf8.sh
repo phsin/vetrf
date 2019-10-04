d577b47e725bf1b294ad8be615e7c86310a7be40
phsin <phsin@kb99.pro>
2019-10-05 01:18:19 +0400

    Общая обработка документов Реализаций и Перемещений refs #3047 Статический анализ модулей SonarQube refs #3028

---
+++ convert_utf8.sh
@@ -0,0 +1,34 @@
+# !/bin/sh
+echo CONVERT src
+rm sonar_src -r -d
+cp -r src sonar_src
+cd sonar_src
+#for i in *.1s; do iconv -f WINDOWS-1251 -t UTF-8 "$i" >tmp; mv tmp "$i"; done
+#for i in *.mdp; do iconv -f WINDOWS-1251 -t UTF-8 "$i" >tmp; mv tmp "$i"; done
+#for i in *.frm; do iconv -f WINDOWS-1251 -t UTF-8 "$i" >tmp; mv tmp "$i"; done
+#for i in *.txt; do iconv -f WINDOWS-1251 -t UTF-8 "$i" >tmp; mv tmp "$i"; done
+find . -type f -name '*.1s' -o -name '*.1s' | while read i
+    do
+    echo converting $i
+    iconv -f WINDOWS-1251 -t UTF-8 "$i" > tmp
+    mv -f tmp "$i.bsl"
+    done
+#find . -type f -name '*.mdp' -o -name '*.mdp' | while read i
+#    do
+#    echo converting $i
+#    iconv -f WINDOWS-1251 -t UTF-8 "$i" > tmp
+#    mv -f tmp "$i.utf"
+#    done
+#find . -type f -name '*.frm' -o -name '*.frm' | while read i
+#    do
+#    echo converting $i
+#    iconv -f WINDOWS-1251 -t UTF-8 "$i" > tmp
+#    mv -f tmp "$i.utf"
+#    done
+
+#find . -type f -name '*.txt' -o -name '*.txt' | while read i
+#    do
+#    echo converting $i
+#    iconv -f WINDOWS-1251 -t UTF-8 "$i" > tmp
+#    mv -f tmp "$i.utf"
+#    done    
