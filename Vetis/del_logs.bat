8a9a26e3a5062185611503c455f74f5eba17ab4e
phsin <phsin@kb99.pro>
2020-02-20 17:37:01 +0400

    Оформление биоотходов и утилизация refs #3382

--- Vetis/del_logs.bat
+++ Vetis/del_logs.bat
@@ -1,7 +1,7 @@
 rem Удаление xml файлов старше 30 дней в каталоге
 
 set offset=30
-set logsDir="C:\1C\Complex\Vetis\logs\"
+set logsDir="logs"
 cd  %logsDir%
 rem for /f "skip=%offset%" %%i in ('dir /o:-d /a:-d /b *.xml') do del %%i
 forfiles /d -%offset% /m *.xml /c "cmd /c echo delete @FDATE @FTIME @PATH && del /f /q @file" 
